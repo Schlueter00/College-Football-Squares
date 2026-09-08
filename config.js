7c675206ee3fb6026999db7c92510fa3f50d898c
Schlueter00 <36335398+Schlueter00@users.noreply.github.com>
2026-09-08 10:24:10 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+const firebaseConfig = {
+    apiKey: "YOUR_NEW_RESTRICTED_API_KEY",
+    authDomain: "your-project-id.firebaseapp.com",
+    databaseURL: "https://your-project-id-default-rtdb.firebaseio.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "your-sender-id",
+    appId: "your-app-id"
+};
